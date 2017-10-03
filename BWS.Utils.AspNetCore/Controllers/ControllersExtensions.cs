45629ecf96e77a872773a6de7be6b66bb6337f54
Wallace Sun <miao17game@qq.com>
2017-10-03 16:37:59 +0800

    create project

---
+++ BWS.Utils.AspNetCore/Controllers/ControllersExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Linq;
+using BWS.Utils.NetCore.Seriailzation.ContractResolvers;
+
+namespace BWS.Utils.AspNetCore.Controllers {
+
+    /// <summary>
+    /// Asp.Net Core Controller Extensions.
+    /// </summary>
+    public static class ControllersExtensions {
+
+        /// <summary>
+        /// return a json result in format name style 'name_style' .
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static JsonResult JsonFormat(this Controller c, dynamic model)
+            => c.Json(model,
+                new JsonSerializerSettings {
+                    ContractResolver = new LowercaseContractResolver(),
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                    Formatting = Formatting.Indented
+                });
+
+        /// <summary>
+        /// Read errors in model state.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetModelErrors(this Controller c)
+            => c.ModelState.Values.Select(i => i.Errors.Select(s => s.ErrorMessage).FirstOrDefault());
+
+        /// <summary>
+        /// Return errors if not nullable, er return 'success'.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="succeed"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static string GetErrorMessage(this Controller c, bool succeed = true, string msg = "")
+            => succeed ? "success" : msg;
+    }
+
+}
---
+++ BWS.Utils.AspNetCore/JsonResult/JsonBase.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BWS.Utils.AspNetCore.JsonResult {
+
+    /// <summary>
+    /// Jsonbase for response.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [JsonObject]
+    public class JsonBase<T> : Dictionary<string, object> where T : struct {
+
+        /// <summary>
+        /// Create jsonbase in default type.
+        /// </summary>
+        public JsonBase() {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException("T must be the enum type.");
+            this.Status = default(T);
+            this.Message = null;
+            this.Content = null;
+        }
+
+        /// <summary>
+        /// Create jsonbase with message.
+        /// </summary>
+        /// <param name="message"></param>
+        public JsonBase(string message) : base() => this.Message = message;
+
+        /// <summary>
+        /// json element with name 'status', type is T (value type).
+        /// </summary>
+        [JsonProperty( "status")]
+        public T Status {
+            get => (T)this["Status"];
+            set => this["Status"] = value;
+        }
+
+        /// <summary>
+        /// json element with name 'msg', type is string.
+        /// </summary>
+        [JsonProperty("msg")]
+        public string Message {
+            get => this["Message"] as string;
+            set => this["Message"] = value;
+        }
+
+        /// <summary>
+        /// json element with name 'content', type is object.
+        /// </summary>
+        [JsonProperty("content")]
+        public object Content {
+            get => this["Content"];
+            set => this["Content"] = value;
+        }
+
+    }
+
+}
