fbea7d014cc02a43e95c597fa5926338f1846e1e
Wallace Sun <miao17game@qq.com>
2017-10-04 09:37:43 +0800

    add xunix project

---
+++ XTEST/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using BWS.Utils.AspNetCore.JsonResult;
+using Newtonsoft.Json;
+
+namespace JsonBase_TEST  {
+
+    enum TestEnum { A, B }
+
+    public class UnitTest1 {
+        [Fact]
+        public void JsonBaseSeriailize_TEST() {
+
+            var jbase = new JsonBase<TestEnum> {
+                Status = TestEnum.A,
+                Message = "aaa",
+                Content = "bbb"
+            };
+            var json = JsonConvert.SerializeObject(jbase);
+
+            Assert.Equal("{\"status\":0,\"msg\":\"aaa\",\"content\":\"bbb\"}", json);
+
+        }
+    }
+}
